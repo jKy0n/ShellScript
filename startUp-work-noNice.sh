7df0ba019899909bd168778c5c5de816795edeeb
jKyon <jk.1989@hotmail.com>
2023-04-20 16:57:30 -0300

    Modify StartUp-work scripts

--- startUp-work-noNice.sh
+++ startUp-work-noNice.sh
@@ -10,21 +10,15 @@
 #
 sudo cpupower frequency-set -g performance &&
 sleep 5 ;
-#sudo apt-get update ;
-gnome-system-monitor &
-kdeconnect-app &
+#nice -n 16 xpad &
 firefox &
 sleep 3 &&
-#/opt/sublime_text/sublime_text &
-atom &
-/home/jkyon/.joplin/Joplin.AppImage &
+vscode &
+~/.joplin/Joplin.AppImage &
 thunderbird &
-xpad &
 sleep 3 &&
 spotify &
-#franz &
-rambox &
-#sleep 30 &&
-#sudo apt-get upgrade -y &&
+discord &
+#nice -n 5 rambox &
 sleep 4m &&
-sudo cpupower frequency-set -g schedutil ;
+sudo cpupower frequency-set -g ondemand ;
--- startUp-work.sh
+++ startUp-work.sh
@@ -10,23 +10,15 @@
 #
 sudo cpupower frequency-set -g performance &&
 sleep 5 ;
-#nice -n 18 gnome-system-monitor &
-#nice -n 18 plasma-systemmonitor &
-#nice -n 18 kdeconnect-app &
-nice -n 16 xpad &
-#nice -n 12 knotes &
+#nice -n 16 xpad &
 firefox &
 sleep 3 &&
-#nice -n 10 /opt/sublime_text/sublime_text &
-code &
-#atom &
-nice -n 10 /home/jkyon/.joplin/Joplin.AppImage &
+vscode &
+nice -n 12 ~/.joplin/Joplin.AppImage &
 nice -n 16 thunderbird &
 sleep 3 &&
-nice -n 4 spotify &
-nice -n 4 discord &
-#sh ~/ShellScript/Spotify-fix.sh ;
-#franz &
+nice -n 5 spotify &
+nice -n 10 discord &
 #nice -n 5 rambox &
 sleep 4m &&
 sudo cpupower frequency-set -g ondemand ;
