199b37b07813e6c571519287a14ca5f35b805041
jKy0n <jk.1989@hotmail.com>
2023-04-21 10:50:20 -0300

    Change nice and order in startUpApps scritpt

--- startUpApps.sh
+++ startUpApps.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 sudo cpupower frequency-set -g performance &&
 sleep 3s ;
-nice -n 10 vscode &
+nice -n 5 vscode &
 #nice -n 18 kdeconnect-app &
 sleep 5s ;
-#nice -n 16 thunderbird &
-#nice -n 12 ~/.joplin/Joplin.AppImage &
+nice -n 16 thunderbird &
+nice -n 10 ~/.joplin/Joplin.AppImage &
 discord &
 nice -n 5 spotify &
 sleep 8s ;
