b09d5dd8750baf5d751aabf0a51a6e7d2a73f01e
John Kennedy Loria Segundo <jk.1989@hotmail.com>
2024-08-16 18:51:39 -0300

    add startUpApps-CrisNote

---
+++ startUpApps-CrisNote.sh
@@ -0,0 +1,13 @@
+!/usr/bin/env bash
+#
+#
+#
+# nice -n 18 kdeconnect-app &
+#sleep 5s ;
+#nice -n 10 obsidian &
+nice -n 10 pavucontrol &
+nice -n 5 spotify &
+krita &
+#discord &
+sleep 2s ;
+firefox &
