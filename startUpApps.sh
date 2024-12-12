6556c52c204a4f8e3de2b41ef548aef49ac3c249
John Kennedy Loria Segundo <jk.1989@hotmail.com>
2024-12-12 19:36:26 -0300

    ~startUpApps

--- startUpApps.sh
+++ startUpApps.sh
@@ -4,14 +4,13 @@
 #
 # sudo cpupower frequency-set -g performance &&
 # sleep 3s ;
-nice -n 5 vscode &
+vscode &
 kdeconnectd &
 nice -n 18 kdeconnect-app &
 # sleep 5s ;
-# nice -n 10 ~/.joplin/Joplin.AppImage &
-nice -n 10 obsidian &
-backintime &
-nice -n 16 pavucontrol &
+nice -n 5 obsidian &
+# backintime &
+nice -n 15 pavucontrol &
 nice -n 5 spotify &
 # pulseeffects &
 discord &
@@ -21,6 +20,3 @@ firefox &
 google-chrome-stable &
 # sleep 2m ;
 # sudo cpupower frequency-set -g ondemand ;
-
-
-# ColorScheme=CatppuccinMacchiatoMauve
