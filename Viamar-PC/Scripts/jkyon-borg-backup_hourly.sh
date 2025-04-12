1132eb37118013b9f78a4354b5140c211f2edfa0
jKy0n <jk.1989@hotmail.com>
2025-04-12 11:02:13 -0300

    add borg script for Viamar-PC

---
+++ Viamar-PC/Scripts/jkyon-borg-backup_hourly.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Pastas/origens a incluir no backup:
+SRC="/home/jkyon /etc /var/log"   # ajuste conforme necessário
+# Repositório do Borg:
+REPO="/mnt/backup/borg"
+
+# Nome do host (para identificar nas backups) e timestamp:
+HOST=$(uname --nodename)
+TIMESTAMP=$(date +'%Y-%m-%d_%H-%M')
+
+# 1. Criação do backup (borg create)
+borg create -v --stats                       \
+    "$REPO::$HOST-$TIMESTAMP"                \
+    $SRC                                     \
+    --exclude-caches                         \
+    --compression zstd                       \
+    --one-file-system
+
+# 2. Prune/expurgo de backups antigos (mantém últimos 24h de hora em hora, últimos 7 dias de diário, etc.)
+borg prune -v "$REPO" --prefix "$HOST-" --show-rc    \
+    --keep-hourly=24   --keep-daily=7   --keep-weekly=4
+
