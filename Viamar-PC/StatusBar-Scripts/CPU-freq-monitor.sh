15de6d0de7be7d53dd356dd8f7ea65a4835903ff
jKy0n <jk.1989@hotmail.com>
2025-06-28 10:32:22 -0300

    Viamar-PC/StatusBar-Scripts/CPU-freq-monitor.sh: set 1 deciamal

--- Viamar-PC/StatusBar-Scripts/CPU-freq-monitor.sh
+++ Viamar-PC/StatusBar-Scripts/CPU-freq-monitor.sh
@@ -2,6 +2,6 @@
 # Frequência média de todos os núcleos em MHz (converte para GHz ao final)
 
 avg=$(awk '/cpu MHz/ { total += $4; n++ }
-           END { if (n>0) printf "%.2f\n", (total/n)/1000 }' /proc/cpuinfo)
+           END { if (n>0) printf "%.1f\n", (total/n)/1000 }' /proc/cpuinfo)
 
 echo "${avg}GHz"
