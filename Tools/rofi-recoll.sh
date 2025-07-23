f6850156e74a95439be28e56580091d5d8962db2
jKy0n <jk.1989@hotmail.com>
2025-07-23 11:03:18 -0300

    make recoll emulate spotlight with rofi

--- Tools/rofi-recoll.sh
+++ Tools/rofi-recoll.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-
-query=$(rofi -dmenu -p "Search Recoll:")
-
-[[ -z "$query" ]] && exit 0
-
-# Execute the search in Recoll, clean output, get only the path
-recollq -t "$query" | sed 's/^file:\/\/\(.*\)/\1/' | rofi -dmenu -i -p "Results:" | xargs -r xdg-open
+#!/usr/bin/env bash
+if [[ $# -eq 0 ]]; then
+  echo -en "\0prompt\x1frecoll:"
+  exit 0
+fi
+q="$1"
+recollq -b -t "$q" | sed 's|^file://||p'
