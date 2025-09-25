5b28ab7341b63e03cb488b621d6860cb9f7ea8d0
jKy0n <jk.1989@hotmail.com>
2025-09-25 10:03:15 -0300

    fastfetch: add PackageManager version checker

---
+++ fastfetch-scripts/fastfetch-packageManager-verion.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# Detectar a distribuição usando /etc/os-release
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    DISTRO=$ID
+else
+    echo "Não foi possível detectar a distribuição."
+    exit 1
+fi
+
+# Função para Arch Linux (pacman)
+check_arch() {
+    # Extrai versões usando pacman -Q
+    pacman_version=$(pacman -Q pacman | awk '{print $2}')
+    paru_version=$(pacman -Q paru | awk '{print $2}')
+
+    # Se bees não estiver instalado, define como "not installed"
+    if [ -z "$paru_version" ]; then
+        paru_version="not installed"
+    fi
+
+    # Saída formatada
+    echo "pacman $pacman_version + bees $paru_version"
+}
+
+# Função para Gentoo (portage)
+check_gentoo() {
+    # Extrair versão do portage
+    portage_version=$(qlist -Iv sys-apps/portage 2>/dev/null)
+    
+    # Verificar se o portage está instalado
+    if [ -z "$portage_version" ]; then
+        echo "Portage não instalado"
+        exit 0
+    fi    
+    
+    # Saída final
+    echo "Portage ${portage_version##*-}"
+}
+
+# Verificar a distribuição e executar o código correspondente
+case $DISTRO in
+    arch)
+        check_arch
+        ;;
+    gentoo)
+        check_gentoo
+        ;;
+    *)
+        echo "Distribuição não suportada: $DISTRO"
+        exit 1
+        ;;
+esac
