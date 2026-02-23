1fadaea5c76ce8bd7231b76bd392abe7dbdab764
jKy0n <jk.1989@hotmail.com>
2026-02-23 09:24:42 -0300

    add new scripts to update and upgrade routines

---
+++ TheseusMachine/upgradePortage.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# filepath: /home/jkyon/ShellScript/TheseusMachine/upgradePortage.sh
+
+# Mostra as atualizações disponíveis no Gentoo
+sudo emerge -vauND @world
---
+++ Viamar-PC/upgradeParu.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# filepath: /home/jkyon/ShellScript/Viamar-PC/upgradeParu.sh
+
+# Verifica se o paru está instalado
+if ! command -v paru &> /dev/null; then
+     echo "Paru não está instalado. Instale com: sudo pacman -S paru"
+     exit 1
+fi
+
+# Atualiza os repositórios e mostra atualizações disponíveis
+paru -Syyuuv --noconfirm
