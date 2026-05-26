7d247492255c1873abe7029bee49022a85d8902a
jKy0n <jk.1989@hotmail.com>
2026-05-26 16:38:35 -0300

    updateParu.sh security change

--- Viamar-PC/Scripts/lock-screen/lock-screen.sh
+++ Viamar-PC/Scripts/lock-screen/lock-screen.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-set -eu
-
-# Apaga os monitores imediatamente
-xset dpms force off
-
-# Janela de tolerância
-sleep 60
-
-# Se ainda estiver idle após a tolerância, trava
-
-exec i3lock --nofork --ignore-empty-password -i /home/jkyon/Pictures/Wallpapers/ArchLinux-Nebula.png
+exec /usr/bin/i3lock --nofork --ignore-empty-password -i /home/jkyon/Pictures/Wallpapers/ArchLinux-Nebula.png
