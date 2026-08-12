6ea8a42e75fcf4aeb3d9fd17d5095c9b83565eed
jKy0n <jk.1989@hotmail.com>
2026-08-12 17:31:08 -0300

    fastfetch minors adjustments

--- fastfetch-scripts/fastfetch-zsh+tmux-version.sh
+++ fastfetch-scripts/fastfetch-zsh+tmux-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-terminal_version=$(alacritty --version | awk '{print $1, $2}')
+terminal_version=$($TERMINAL --version | awk '{print $1, $2}')
 zsh_version=$(zsh --version | awk '{print $1, $2}')
 tmux_version=$(tmux -V | awk '{print $1, $2}')
 echo "$terminal_version + $zsh_version + $tmux_version"
