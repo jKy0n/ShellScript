f5aa067e31aaeea1c53af6f20c6f3427407d5d33
jKy0n <jk.1989@hotmail.com>
2026-08-25 17:19:24 -0300

    arquivando scripts antigos ou não usados

--- Debian/tools/upgrade-apt.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# filepath: /home/jkyon/ShellScript/Debian/tools
-
-# Mostra as atualizações disponíveis no Gentoo
-sudo apt update && sudo apt upgrade
--- niri/rename-workspace.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-NOME="$(echo "" | walker --dmenu --inputonly --placeholder "Nome do workspace:")"
-[ -n "$NOME" ] && niri msg action set-workspace-name "$NOME"
